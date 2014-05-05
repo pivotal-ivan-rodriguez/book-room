2a473f022fe416d9298d3843f8c721304360f2a1
Doron Yakovlev Golani <doron.golani@xtremelabs.com>
2014-05-05 14:13:13 -0400

    IvanR - First commit

---
+++ Book Room/BRHomeViewController.m
@@ -0,0 +1,44 @@
+//
+//  BRViewController.m
+//  Book Room
+//
+//  Created by DX169-XL on 2014-05-05.
+//  Copyright (c) 2014 Pivotal Labs. All rights reserved.
+//
+
+#import "BRHomeViewController.h"
+//#import "GTMOAuth2ViewControllerTouch.h"
+
+@interface BRHomeViewController ()
+
+@end
+
+@implementation BRHomeViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+//
+//    static NSString *const kKeychainItemName = @"Book a Room";
+//
+//    NSString *kMyClientID = @"776916698629-jm882d2nnh738lo5qio3quqehej4i4a3.apps.googleusercontent.com";
+//    NSString *kMyClientSecret = @"8hTo-W7xyeQhVO3domrWM7Ys";
+//
+//    NSString *scope = @"https://www.googleapis.com/auth/calendar";
+//
+//    GTMOAuth2ViewControllerTouch *viewController = [[GTMOAuth2ViewControllerTouch alloc] initWithScope:scope clientID:kMyClientID clientSecret:kMyClientSecret keychainItemName:kKeychainItemName delegate:self finishedSelector:@selector(viewController:finishedWithAuth:error:)];
+//    
+//    [[self navigationController] pushViewController:viewController animated:YES];
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+//- (void)viewController:(GTMOAuth2ViewControllerTouch *)viewController finishedWithAuth:(GTMOAuth2Authentication *)auth error:(NSError *)error {
+//
+//}
+
+@end
